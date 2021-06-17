b2451d619a7c3eee0d3748727dbf3db3e93277b1
mrbrooks06 <77938810+mrbrooks06@users.noreply.github.com>
2021-06-18 01:13:24 +0300

    burak1

--- src/Burak.java
+++ src/Burak.java
@@ -1,2 +1,15 @@
 public class Burak {
+
+    //Neyzen Tevfik bir gün camide hocanın vaazını dinler. Hoca cemaate herkesin dinin gereklerini yerine getirmesi gerektiğini, cennette herkese çok güzel huriler verileceğinden ve bu hurilerle ne yapmak isterlerse yapabileceklerini anlatır. Ertesi günkü vaazda Neyzen hocaya sorar:
+    //
+    //— Hocam cennette şarap olacak mı?
+    //
+    //Hoca bu soruya çok sinirlenir, başlar Neyzen’i zındık, kafir, iblis gibi dini motiflerle haşlamaya ve sorar:
+    //
+    //— Bre zındık, cenneti meyhane mi sandın?
+    //
+    //Neyzen istifini bozmadan önceki günü hatırlatır:
+    //
+    //— E hoca, dün cenneti kerhane yaptın.
+
 }
